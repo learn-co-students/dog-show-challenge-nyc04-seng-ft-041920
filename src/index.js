c187c4515852eddc8e8674504cc5eb6129aebeaf
iamkevn <iamkevnrodriguez@gmail.com>
2020-07-06 18:08:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
+const tableBody = document.querySelector("#table-body")
+const dogForm = document.querySelector("#dog-form")
+
+dogForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const updateDog = {
+    name: e.target.name.value,
+    breed: e.target.breed.value,
+    sex: e.target.sex.value
+    }
+    updateDog(e.target.dataset.id, updateDog).then(console.log)
 
 })
+
+function populateDogForm(dog) {
+    dogForm.name.value = dog.name
+    dogForm.breed.value = dog.breed
+    dogForm.sex.value = dog.sex
+
+    dogForm.dataset.id = dog.id
+}
+
+function renderDogRow(dog) {
+    const dogRow = document.createElement("tr")
+    dogRow.innerHTML = `
+    <td>${dog.name}</td> 
+    <td>${dog.breed}</td> 
+    <td>${dog.sex}</td> 
+    <td><button>Edit</button></td>
+    `
+    const button = dogRow.querySelector("button")
+    button.addEventListener('click',() => {
+        populateDogForm(dog)
+    })
+
+    tableBody.append(dogRow)
+}
+
+
+function renderAllDogs(dogs) {
+    dogs.forEach(renderDogRow)
+}
+
+
+getAllDogs().then(renderAllDogs)
+
